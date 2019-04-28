a51414df7196c55d70c0267fa2d601b56f2a8fe9
Shawn Lower <shawn@shawnlower.com>
2019-04-28 15:37:14 -0400

    update models

---
+++ src/app/models/property.ts
@@ -0,0 +1,7 @@
+export class Property {
+  iri: string;
+  name: string;
+  datatype: string;
+  description: string;
+  properties?: Array<any>;
+}
--- src/app/models/type.ts
+++ src/app/models/type.ts
@@ -1,4 +1,8 @@
+import { Property } from './property';
+
 export class Type {
+  iri: string;
   name: string;
   description: string;
+  properties?: Array<Property>;
 }
