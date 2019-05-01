0bb0a9a2776b144ce31f0dc7873eabf6077f29b8
Shawn Lower <shawn@shawnlower.com>
2019-05-01 00:03:29 -0400

    Add dialog for adding a new type

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { HttpClientModule }    from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,27 +9,42 @@ import { TypesListComponent } from './types-list/types-list.component';
 import { TypeEditorComponent } from './type-editor/type-editor.component';
 import { TypeDetailComponent } from './type-detail/type-detail.component';
 
-import {MatToolbarModule, MatIconModule, MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatToolbarModule, MatIconModule, MatInputModule, MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { MatDialogModule } from '@angular/material';
+import { ItemEditorComponent } from './item-editor/item-editor.component';
+import { DialogNewItemComponent } from './dialog-new-item/dialog-new-item.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule} from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
     AppComponent,
     TypesListComponent,
     TypeEditorComponent,
-    TypeDetailComponent
+    TypeDetailComponent,
+    ItemEditorComponent,
+    DialogNewItemComponent,
   ],
   imports: [
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCheckboxModule,
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatDialogModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    DialogNewItemComponent
+  ]
 })
 export class AppModule { }
--- src/app/type-detail/type-detail.component.ts
+++ src/app/type-detail/type-detail.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 
 import { Observable, of } from 'rxjs';
 import { switchMap, map, tap } from 'rxjs/operators';
 
+import { DialogNewItemComponent } from '../dialog-new-item/dialog-new-item.component';
 import { LtpService } from '../services/ltp.service';
 import { Type } from '../models/type';
 import { Property } from '../models/property';
@@ -11,18 +13,38 @@ import { Property } from '../models/property';
 @Component({
   selector: 'app-type-detail',
   templateUrl: './type-detail.component.html',
-  styleUrls: ['./type-detail.component.css']
+  styleUrls: ['./type-detail.component.css'],
 })
 export class TypeDetailComponent implements OnInit {
 
   type$: Observable<any>;
+  name: string;
 
   constructor(
+    public dialog: MatDialog,
     private route: ActivatedRoute,
     private router: Router,
     private ltpService: LtpService
   ) { }
 
+
+  instantiate(): void {
+    /*
+    this.type$.subscribe(o =>
+      this.router.navigate(['/items/new', o.name]));
+      */
+    // const dialogRef = this.dialog.open(DialogTest, {
+    const dialogRef = this.dialog.open(DialogNewItemComponent, {
+      width: '250px',
+      data: {type$: this.type$, name: this.name}
+    });
+
+    dialogRef.afterClosed().subscribe(results => {
+      console.log('You entered', results);
+      this.ltpService.newItem(results.name).subscribe(item =>
+        this.router.navigate(['/item/', item.id]));
+    });
+  }
 
   ngOnInit() {
     const id = this.route.snapshot.params.id;
@@ -45,3 +67,4 @@ export class TypeDetailComponent implements OnInit {
   }
 
 }
+
