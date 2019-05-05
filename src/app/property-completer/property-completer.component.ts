18033b0a7389ea1d5cd25d07adc9c9177e6d2830
Shawn Lower <shawn@shawnlower.com>
2019-05-05 13:37:16 -0400

    Add property-completer component

---
+++ src/app/property-completer/property-completer.component.ts
@@ -0,0 +1,79 @@
+import { FormControl } from '@angular/forms';
+import { ElementRef, ViewChild } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+import { filter, last, startWith, map, switchMap } from 'rxjs/operators';
+
+import { Component, Input, OnInit } from '@angular/core';
+import { Item } from '../models/item';
+import { Property } from '../models/property';
+import { LtpService } from '../services/ltp.service';
+
+@Component({
+  selector: 'app-property-completer',
+  templateUrl: './property-completer.component.html',
+  styleUrls: ['./property-completer.component.css']
+})
+export class PropertyCompleterComponent implements OnInit {
+
+  @Input() item: Observable<Item>;
+
+  @ViewChild('propVal')
+  propVal: ElementRef;
+
+  propertyCtrl = new FormControl();
+  propertyValueCtrl = new FormControl();
+
+  properties$: Observable<Property[]>;
+  filteredProperties: Observable<Property[]>;
+
+  property$: Observable<Property>;
+
+  constructor(
+    private ltpService: LtpService,
+  ) { }
+
+  ngOnInit() {
+    if (this.item) {
+      this.item.subscribe(item => {
+        this.properties$ = this.ltpService.getProperties(item.itemType);
+      });
+    }
+
+    this.propertyCtrl.valueChanges.pipe(
+      switchMap(query => this.filterProperties(query)))
+            .subscribe(properties => {
+              console.log('results', properties);
+              this.filteredProperties = of(properties);
+            })
+  }
+
+  private filterProperties(query): Observable<Property[]> {
+    const filterValue = query.toLowerCase();
+    console.log('filter', query, filterValue);
+    return this.properties$.pipe(
+      map(props => {
+        console.log('filterMap', props.filter(p => p.name.indexOf(filterValue)));
+        return props.filter(p => p.name.indexOf(filterValue) === 0 ||
+                                 p.description.indexOf(filterValue) >= 0);
+      })
+    );
+  }
+
+  onEnter(evt: any) {
+
+    if (evt !== undefined && evt.source.selected) {
+      const name = evt.source.value;
+      console.log('onEnter', name);
+      this.property$ = this.properties$.pipe(
+        last(),
+        map(properties => properties.filter(p => p.name === name).shift())
+      )
+      this.property$.subscribe(property => {
+        this.propertyCtrl.disable();
+        this.propVal.nativeElement.focus();
+      });
+    }
+
+  }
+}
