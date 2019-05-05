ef7b2f9e996d70d5f4314bae66a944361c879dd5
Shawn Lower <shawn@shawnlower.com>
2019-05-05 13:20:42 -0400

    Add snack-bar for messages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,17 @@ import { TypeEditorComponent } from './type-editor/type-editor.component';
 import { TypeDetailComponent } from './type-detail/type-detail.component';
 
 import {MatToolbarModule, MatIconModule, MatInputModule, MatButtonModule, MatCheckboxModule} from '@angular/material';
-import { MatDialogModule } from '@angular/material';
+import { MatDialogModule, MatSnackBarModule, } from '@angular/material';
 import { ItemEditorComponent } from './item-editor/item-editor.component';
 import { DialogNewItemComponent } from './dialog-new-item/dialog-new-item.component';
 
+import { StoreModule } from '@ngrx/store';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule} from '@angular/material/form-field';
+import { SnackBarComponent } from './snack-bar/snack-bar.component';
+
+import { reducers, metaReducers } from './reducers';
 
 @NgModule({
   declarations: [
@@ -25,6 +30,7 @@ import { MatFormFieldModule} from '@angular/material/form-field';
     TypeDetailComponent,
     ItemEditorComponent,
     DialogNewItemComponent,
+    SnackBarComponent,
   ],
   imports: [
     AppRoutingModule,
@@ -39,12 +45,16 @@ import { MatFormFieldModule} from '@angular/material/form-field';
     MatIconModule,
     MatInputModule,
     MatFormFieldModule,
+    MatSnackBarModule,
     MatToolbarModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+
   ],
   providers: [],
   bootstrap: [AppComponent],
   entryComponents: [
-    DialogNewItemComponent
+    DialogNewItemComponent,
+    SnackBarComponent,
   ]
 })
 export class AppModule { }
